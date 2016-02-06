254539033fe94d43463c5a6e5515eead19767801
ruping <rs3412@c2b2.columbia.edu>
2016-02-05 20:50:17 -0800

    titanOption

--- lib/cnaCalling.pm
+++ lib/cnaCalling.pm
@@ -10,7 +10,7 @@ sub runTitan {
 
   my ($class, $titanRBin, $PATH, $sampleName, $alleleCount, $tumorWig, $normalWig, $gcWig, $mapWig, $plp, $plpe, $nc, $ncm, $exons) = @_;
 
-  my $cmd = "$titanRBin $PATH $sampleName $alleleCount $tumorWig $normalWig $gcWig $mapWig $plp, $plpe, $nc, $ncm, $exons";
+  my $cmd = "$titanRBin $PATH $sampleName $alleleCount $tumorWig $normalWig $gcWig $mapWig $plp $plpe $nc $ncm $exons";
 
   return $cmd;
 
