211d1474c26c1ccfac8446e0c57bdc31ac2de499
ruping <rs3412@c2b2.columbia.edu>
2016-06-01 14:00:47 -0700

    skip

--- DTrace.pl
+++ DTrace.pl
@@ -495,9 +495,9 @@ if (exists($runlevel{$runlevels}) or exists($runTask{'mapping'}) or exists($runT
     }
 
     if ((-s "$irBam" and !(-s "$brBam")) or exists($runTask{'BaseRecalibration'})) { #base recalibration
+      my $cmd;
       if ($options{'skipTask'} !~ /BaseRecalibration/) {             #if skipped
         my $brTable = $irBam.".baseRecal.table";
-        my $cmd;
         unless (-s "$brTable") {
           $cmd = bwaMapping->BaseRecalibration($confs{'gatkBin'}, $irBam, $confs{'GFASTA'}, $confs{'muTectDBSNP'}, $confs{'KNOWNINDEL1'}, $confs{'KNOWNINDEL2'}, $brTable);
           RunCommand($cmd,$options{'noexecute'},$options{'quiet'});
@@ -505,7 +505,7 @@ if (exists($runlevel{$runlevels}) or exists($runTask{'mapping'}) or exists($runT
         $cmd = bwaMapping->BaseRecalibrationPrint($confs{'gatkBin'}, $irBam, $confs{'GFASTA'}, $brTable, $brBam);
         RunCommand($cmd,$options{'noexecute'},$options{'quiet'});
       } else {
-        my $cmd = "mv $irBam $brBam";
+        $cmd = "mv $irBam $brBam";
         RunCommand($cmd,$options{'noexecute'},$options{'quiet'});
       }
       $cmd = bwaMapping->bamIndex($confs{'samtoolsBin'}, $brBam); #index it
