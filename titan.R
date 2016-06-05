231e3260c33deb6e44c498c7d6881a083e6cbe0a
ruping <rs3412@c2b2.columbia.edu>
2016-06-04 21:49:13 -0700

    titan2seg

--- titan.R
+++ titan.R
@@ -165,7 +165,12 @@ runTitan <- function(sampleName, snpFile, tumWig, normWig, gc, map, plp, plpe, n
 }
 
 
+
 titancna2seg <- function(titanresult,titanparams) {
+
+  major_cn_code <- c(0,1,2,1,3,2,4,3,2,5,4,3,6,5,4,3,7,6,5,4,8,7,6,5,4)
+  minor_cn_code <- c(0,0,0,1,0,1,0,1,2,0,1,2,0,1,2,3,0,1,2,3,0,1,2,3,4)
+
   ploidy <- round(tail(titanparams$phi,1),3)
   n <- round(tail(titanparams$n,1),3)
 
@@ -175,7 +180,7 @@ titancna2seg <- function(titanresult,titanparams) {
   titanresult$CopyNumber <- as.numeric(titanresult$CopyNumber)
   titanresult$CellularPrevalence <- as.numeric(titanresult$CellularPrevalence)
   titanresult$ClonalCluster[is.na(titanresult$ClonalCluster)] <- 0
-  
+
   cp2 <- c(which(titanresult$TITANstate[-1] != titanresult$TITANstate[-nrow(titanresult)] | 
                    titanresult$Chr[-1] != titanresult$Chr[-nrow(titanresult)] | 
                      titanresult$ClonalCluster[-1] !=  titanresult$ClonalCluster[-nrow(titanresult)]),
@@ -188,6 +193,8 @@ titancna2seg <- function(titanresult,titanparams) {
                     num.mark=cp2-cp1+1,
                     seg.mean=titanresult$LogRatio[cp1],
                     copynumber=titanresult$CopyNumber[cp1],
+                    minor_cn=minor_cn_code[as.integer(titanresult$TITANstate[cp1])+1],
+                    major_cn=major_cn_code[as.integer(titanresult$TITANstate[cp1])+1],
                     allelicratio=titanresult$AllelicRatio[cp1],
                     LOHcall=titanresult$TITANcall[cp1],
                     cellularprevalence=titanresult$CellularPrevalence[cp1],
@@ -196,15 +203,30 @@ titancna2seg <- function(titanresult,titanparams) {
   for (j in 1:length(cp1)) {
     cnv$seg.mean[j] <- mean(titanresult$LogRatio[cp1[j]:cp2[j]])
     cnv$allelicratio[j] <- mean(0.5+abs(0.5-titanresult$AllelicRatio[cp1[j]:cp2[j]]))
+    if (j < length(cp1)) {
+      if (titanresult$Chr[cp2[j]] == titanresult$Chr[cp1[j+1]]) {
+        cnv$loc.end[j] <- round((cnv$loc.end[j]+cnv$loc.start[j+1])/2)
+      }
+    }
+    if (j > 1) {
+      if (titanresult$Chr[cp1[j]] == titanresult$Chr[cp2[j-1]]) {
+        cnv$loc.start[j] <- cnv$loc.end[j-1]+1
+      }
+    }
   }
+ 
   cnv$logcopynumberratio <- log2(((cnv$copynumber - 2) * cnv$cellularprevalence + 2)/2)
   cnv$logcopynumberratio[is.na(cnv$logcopynumberratio)] <- 0
-  ##cnv$seg.mean.2 <- cnv$seg.mean
-  for (i in c(5,7,12)) {
+
+  for (i in c(5,12)) {
     cnv[[i]] <- round(cnv[[i]],3)
   }
   return(cnv)
+
 }
+
+
+
 
 #process input par
 if (plpe == "FALSE") {
