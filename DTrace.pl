b6dc4c62ba59f23d44c1862a4b122ead4615ddf3
ruping <rs3412@c2b2.columbia.edu>
2016-11-03 13:12:43 -0700

    indel

--- DTrace.pl
+++ DTrace.pl
@@ -1007,9 +1007,7 @@ if (exists($runlevel{$runlevels}) or exists($runTask{'recheck'})) {
     unless (-s "$vcfOut" or -s "$vcfOutSorted" or -s "$vcfMultiAnnoMod") {
       my $cmd = snvCalling->strelkaCalling($confs{'strelkaBin'}, $normalBam, $finalBam, $confs{'GFASTA'}, $confs{'strelkaConfig'}, $strelkaOutDir);     #configure
       RunCommand($cmd,$options{'noexecute'},$options{'quiet'});
-      $cmd = "cd $strelkaOutDir";                                                                                                                       #change dir
-      RunCommand($cmd,$options{'noexecute'},$options{'quiet'});
-      $cmd = "make -j $options{'threads'}";                                                                                                             #call indel
+      $cmd = "make -C $strelkaOutDir -j $options{'threads'}";                                                                                           #call indel
       RunCommand($cmd,$options{'noexecute'},$options{'quiet'});
     }
 
