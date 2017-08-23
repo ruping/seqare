123904f5e688ac034f20c6a9e31220342a47942d
ruping <rs3412@c2b2.columbia.edu>
2017-08-23 14:52:50 -0700

    chrs

--- bed2wig.pl
+++ bed2wig.pl
@@ -14,17 +14,24 @@ print STDERR Dumper(\@chrs);
 
 open IN, "$bed";
 my %bed;
+my $chrprefix = '';
 while ( <IN> ){
   chomp;
   my ($chr, $start, $end, $tags, $starts) = split /\t/;
   push (@{$bed{$chr}}, $starts);
+  if ($chrprefix eq '' and $chr =~ /^([Cc][Hh][Rr])/){
+    $chrprefix = $1;
+  }
 }
 close IN;
 
 print STDERR "all bed line stored\n";
 
 
-foreach my $chr (@chrs){
+foreach my $chr (@chrs) {
+  if ($chrprefix ne ''){
+    $chr = $chrprefix.$chr;
+  }
   print "fixedStep chrom=$chr start=1 step=1000 span=1000\n";
   foreach my $starts (@{$bed{$chr}}){
     print "$starts\n";
