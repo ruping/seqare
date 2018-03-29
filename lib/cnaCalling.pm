4973b9615e9cbd82500908604706018e4eece4a1
ruping <rs3412@c2b2.columbia.edu>
2018-03-28 22:33:09 -0700

    transtate

--- lib/cnaCalling.pm
+++ lib/cnaCalling.pm
@@ -8,9 +8,9 @@ use strict;
 
 sub runTitan {
 
-  my ($class, $RscriptBin, $titanRBin, $PATH, $pbin, $sampleName, $alleleCount, $tumorWig, $normalWig, $gcWig, $mapWig, $plp, $plpe, $nc, $ncm, $sym, $exons) = @_;
+  my ($class, $RscriptBin, $titanRBin, $PATH, $pbin, $sampleName, $alleleCount, $tumorWig, $normalWig, $gcWig, $mapWig, $plp, $plpe, $nc, $ncm, $sym, $transtate, $tranclone, $exons) = @_;
 
-  my $cmd = "$RscriptBin $titanRBin $PATH $pbin $sampleName $alleleCount $tumorWig $normalWig $gcWig $mapWig $plp $plpe $nc $ncm $sym $exons";
+  my $cmd = "$RscriptBin $titanRBin $PATH $pbin $sampleName $alleleCount $tumorWig $normalWig $gcWig $mapWig $plp $plpe $nc $ncm $sym $transtate $tranclone $exons";
 
   return $cmd;
 
